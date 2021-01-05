5bce593ca11c82b063e84c91bb1a099ce47ca668
Igataka <61051616+igataka@users.noreply.github.com>
2021-01-05 21:02:54 +0900

    Installl Typescript

--- webpack.config.js
+++ webpack.config.js
@@ -15,13 +15,22 @@ module.exports ={
   module:{
     rules:[
       {
+        test: /\.(ts|tsx)/,
+        exclude:/node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+      {
         test:/\.vue/,
         exclude:/node_modules/,
         use: [
           {
             loader: 'vue-loader',
           },
-        ]
+        ],
       },
       {
         test:/\.js/,
