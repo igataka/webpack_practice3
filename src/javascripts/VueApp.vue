0d70d81deee92f1351f836d4d8d575246e7f540c
Igataka <61051616+igataka@users.noreply.github.com>
2021-01-05 16:11:10 +0900

    ES6,React,Vue,etc

---
+++ src/javascripts/VueApp.vue
@@ -0,0 +1,22 @@
+const { Template } = require("webpack");
+
+<template>
+  <p>{{message}}</p>
+</template>
+
+<script>
+export default {
+  data: () =>{
+    console.log('Vuejs is instaled!');
+    return{
+      message: 'Hey,this is Vue!',
+    };
+  },
+};
+</script>
+
+<style scoped>
+p{
+  color:green;
+}
+</style>
--- src/javascripts/my.js
+++ src/javascripts/my.js
@@ -1,3 +1,6 @@
 export default ()=>{
+  const obj ={a:1,b:2};
+  const newobj ={...obj, c:3}
   console.log('this is module');
+  console.log('this is module',newobj);
 }
